2da225b8e3d8854c46d4c919b6989393e8c1aa41
Yulia Tarima <yulia.tarima@gmail.com>
2020-03-17 16:05:59 -0700

    Updates

---
+++ precache-manifest.30901b961f6144a2bb6418b9ff43a538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2f4638f0b7ec2d0a57441580e94d6a",
+    "url": "/ReactJS-Redux-Todo-Class-Based/index.html"
+  },
+  {
+    "revision": "8a6f655ced932e963ed1",
+    "url": "/ReactJS-Redux-Todo-Class-Based/static/css/main.d5da5a67.chunk.css"
+  },
+  {
+    "revision": "ad5f31dddd0d514acbca",
+    "url": "/ReactJS-Redux-Todo-Class-Based/static/js/2.d416d9a6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ReactJS-Redux-Todo-Class-Based/static/js/2.d416d9a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a6f655ced932e963ed1",
+    "url": "/ReactJS-Redux-Todo-Class-Based/static/js/main.5e8a55ee.chunk.js"
+  },
+  {
+    "revision": "d93caadbb7ab608c1e9b",
+    "url": "/ReactJS-Redux-Todo-Class-Based/static/js/runtime-main.89b7512e.js"
+  }
+]);
